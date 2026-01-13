f6837f40e852b23e82be393bfa06ac32a28e9856
et118 <pekdator@gmail.com>
2026-01-13 20:47:45 +0100

    Restructured main a bit, and added content to README.md

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 
 # CPP-Editor
-CPP-Editor is a terminal based user interface implementing a simple editor, inspired by VSCode.
+CPP-Editor is a proof of concept terminal based user interface (TUI) implementing an explorer, editor and terminal.
+![img.png](img.png)
+
+## Features
+* Automatic scaling
+* Loading all text based files
+* Saving all text based files
+* Running .py files
+* Keyboard based text editing
+* Mouse based explorer menu
+* Autosaving on crash
+
+## Shortcomings
+* HEAVILY underoptimized (had no time for that). This makes larger files (around a kilobyte) too slow to run even on a good pc, and might cause freezing during the time it takes to render.
+* Can't type all characters
+* No copy pasting
+* No mouse scrolling (though the editor supports keyboard based scrolling)
+* Can't display tabs or certain non-monospace characters
+* Only supports running python files
+* Doesnt support displaying the error pipe when running programs
+* It supports colors, but i didnt have time to add any
+* No way to show notifications for status messages (like if a file is unavailable)
--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
-#include "include/BorderRenderers/SimpleBorderRenderer.h"
 #include <iostream>
 #include <thread>
-#include <sys/ioctl.h>
-
+#include "include/Util/Content.h"
 #include "include/IO/TerminalIO.h"
 #include "include/Windows/RecursiveWindow.h"
 #include "include/Windows/FileExplorerWindow.h"
 #include "include/Windows/EditorWindow.h"
-#include "include/Util/Content.h"
-#include "include/Util/StringUtils.h"
 #include "include/Windows/TerminalWindow.h"
 
 std::filesystem::path resolvePath(const std::string& path) {
@@ -19,9 +15,9 @@ std::filesystem::path resolvePath(const std::string& path) {
         return std::filesystem::path{std::getenv("HOME")};
     }
     return std::filesystem::path{path};
-
 }
-static RecursiveWindow mainWindow{};
+
+static RecursiveWindow mainWindow{}; //Yes, this is our only public static variable, just so it can be accessed from the callback below
 void onResize(unsigned int width, unsigned int height) {
     mainWindow.getWindowDimensions().setContentAreaSize({width,height - 1});
     TerminalIO::clearTerminal();
@@ -42,6 +38,7 @@ int main(int argc, char* argv[]) {
     subWindow.setHorizontal(true);
     subWindow.addWindow(&explorerWindow);
     subWindow.addWindow(&editorWindow);
+
     subWindow.getWindowDimensions().setMinSize({0,0});
     explorerWindow.getWindowDimensions().setMaxSize({20,0});
     terminalWindow.getWindowDimensions().setMaxSize({0,10});
@@ -50,15 +47,11 @@ int main(int argc, char* argv[]) {
     mainWindow.addWindow(&subWindow);
     mainWindow.addWindow(&terminalWindow);
 
-    mainWindow.getWindowDimensions().setContentAreaSize({51,25});
-    mainWindow.tick();
-
     termios original = TerminalIO::takeControlOfTerminal();
-
     TerminalIO::bindResizeCallback(onResize);
 
     struct winsize w = TerminalIO::getWindowSize();
-    onResize(w.ws_col, w.ws_row); //Call manually on program start to get initial window size
+    onResize(w.ws_col, w.ws_row); //Call one time manually on program start to get initial window size
     TerminalIO::clearTerminal();
 
     //Safetynet for exceptions that i might have forgotten to handle
@@ -74,7 +67,7 @@ int main(int argc, char* argv[]) {
                 std::cout << content.getLine(i) << "\n";
             }
             mainWindow.tick();
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            std::this_thread::sleep_for(std::chrono::milliseconds(10)); //Arbitrary sleep value
         }
     } catch (const std::exception& e) {
         std::cout << "Exception Caught. Trying to autosave" << std::endl;
